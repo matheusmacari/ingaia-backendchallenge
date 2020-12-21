8decf5ee2939e3aa8acbedb483ea53c957e2b3b7
matheus.macari <matheus.macari@senior.com.br>
2020-12-21 15:29:10 -0300

    Ajustada URL para API1 apontando para a aplicação publicada.

--- app/api/api.js
+++ app/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class Api {
     async GetValorMetroQuadrado(){
 
-        const dadosApi = await axios.get('http://localhost:3333/api1');
+        const dadosApi = await axios.get('https://ingaia-challenge-backend.herokuapp.com/api1');
 
         if (dadosApi.status != 200){
             return res.status(500).json({error:"API1 (custo do M²) está indisponível."});
